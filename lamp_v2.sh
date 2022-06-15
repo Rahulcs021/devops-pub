7081339a19729d5f9c51570c3e7bead32d4af4a5
Rahul <rahulkumar.mac021@gmail.com>
2022-06-16 00:29:38 +0530

    updated with new php and mysql version

--- lamp_v2.sh
+++ lamp_v2.sh
@@ -12,11 +12,22 @@ fi
 
 sudo apt update
 sudo apt-get install -y apache2
-sudo ufw allow in "Apache Full"
+
 sudo a2enmod rewrite headers ssl
 
 sudo apt-get install -y mysql-server
-sudo mysql_secure_installation #installing MYSQL
+#sudo mysql_secure_installation #installing MYSQL
+# Make sure that NOBODY can access the server without a password
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'"
+# Kill the anonymous users
+mysql -e "DROP USER ''@'localhost'"
+# Because our hostname varies we'll use some Bash magic here.
+mysql -e "DROP USER ''@'$(hostname)'"
+# Kill off the demo database
+mysql -e "DROP DATABASE test"
+# Make our changes take effect
+mysql -e "FLUSH PRIVILEGES"
+# Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
 
 sudo apt-get -y update
 sudo add-apt-repository ppa:ondrej/php #adding PHP repository
@@ -25,12 +36,11 @@ echo
 echo "#############################################################################################################"
 echo "                                  Which PHP version you want to install                                        "
 echo "#############################################################################################################"
-echo "Press 1 for PHP5.6"
-echo "Press 2 for PHP7.0"
-echo "Press 3 for PHP7.1"
-echo "Press 4 for PHP7.2"
-echo "Press 5 for PHP7.3"
-echo "Press 6 for PHP7.4"
+echo "Press 1 for PHP7.2"
+echo "Press 2 for PHP7.3"
+echo "Press 3 for PHP7.4"
+echo "Press 4 for PHP8.0"
+echo "Press 5 for PHP8.1"
 read -r option
 
 # switch case for selecting PHP version
@@ -38,29 +48,25 @@ read -r option
 case $option in
 
 	1)
-		echo "You chose PHP7.0"
-		sudo apt-get install -y php7.0 libapache2-mod-php7.0 php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-curl php7.0-intl php7.0-xsl php7.0-mbstring php7.0-zip php7.0-bcmath php7.0-iconv php7.0-soap
-		;;
-	2)
-		echo "You chose PHP7.1"
-		sudo apt-get install -y php7.1 libapache2-mod-php7.1 php7.1-common php7.1-gd php7.1-mysql php7.1-mcrypt php7.1-curl php7.1-intl php7.1-xsl php7.1-mbstring php7.1-zip php7.1-bcmath php7.1-iconv php7.1-soap
-		;;
-	3)
 		echo "You chose PHP7.2"
 		sudo apt-get install -y php7.2 libapache2-mod-php7.2 php7.2-common php7.2-gd php7.2-mysql php7.2-curl php7.2-intl php7.2-xsl php7.2-mbstring php7.2-zip php7.2-bcmath php7.2-iconv php7.2-soap
 		;;
-	4)
+	2)
 		echo "You chose PHP7.3"
 		sudo apt-get install -y php7.3 libapache2-mod-php7.3 php7.3-common php7.3-gd php7.3-mysql php7.3-mcrypt php7.3-curl php7.3-intl php7.3-xsl php7.3-mbstring php7.3-zip php7.3-bcmath php7.3-iconv php7.3-soap
 		;;
-	5)
+	3)
 		echo "You chose PHP7.4"
 		sudo apt-get install -y php7.4 libapache2-mod-php7.4 php7.4-common php7.4-gd php7.4-mysql php7.4-mcrypt php7.4-curl php7.4-intl php7.4-xsl php7.4-mbstring php7.4-zip php7.4-bcmath php7.4-iconv php7.4-soap
 		;;
-	6)
+	4)
 		echo "You chose PHP8.0"
 		sudo apt-get install -y php8.0 libapache2-mod-php8.0 php8.0-common php8.0-gd php8.0-mysql php8.0-mcrypt php8.0-curl php8.0-intl php8.0-xsl php8.0-mbstring php8.0-zip php8.0-bcmath php8.0-iconv php8.0-soap
-		
+		;;
+	5)
+		echo "You chose PHP8.1"
+		sudo apt-get install -y php8.1 libapache2-mod-php8.1 php8.1-common php8.1-gd php8.1-mysql php8.1-mcrypt php8.1-curl php8.1-intl php8.1-xsl php8.1-mbstring php8.1-zip php8.1-bcmath php8.1-iconv php8.1-soap
+		;;
 	*)
 		echo "Selected option is not available"
 		;;
