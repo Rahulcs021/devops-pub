46bf6c762f8481e7c9824827a9b22429337e370f
Rahulcs021 <36921300+Rahulcs021@users.noreply.github.com>
2023-08-26 05:57:14 +0530

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Docker script for ubuntu operating system
+# Written By RAHUL
+# Last Modified 26th August 2023
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+sudo usermod -aG docker ${USER}
