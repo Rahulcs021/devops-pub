bc918bbaa3f7ccfb2b8ccd240a6f589f6d5f5c80
Rahulcs021 <36921300+Rahulcs021@users.noreply.github.com>
2023-12-15 11:09:47 +0530

    Create gen_cm_env.sh

---
+++ scripts/gen_cm_env.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z "$ENV_VAR" ]; then
+  echo "Environment variable ENV_VAR is not set."
+  exit 1
+fi
+
+configmap_name="example-configmap"
+configmap_yaml="configmap.yaml"
+
+# Split the ENV_VAR into individual key-value pairs
+IFS='|' read -ra pairs <<< "$ENV_VAR"
+
+# Generate ConfigMap YAML
+cat <<EOF > "$configmap_yaml"
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: $configmap_name
+data:
+EOF
+
+# Loop through each key-value pair, extract keys and values, and append to ConfigMap YAML
+for pair in "${pairs[@]}"; do
+  IFS=':' read -ra kv <<< "$pair"
+  key="${kv[0]}"
+  value="${kv[1]}"
+  echo "  $key: $value" >> "$configmap_yaml"
+done
+
+echo "ConfigMap YAML file $configmap_yaml created successfully!"
